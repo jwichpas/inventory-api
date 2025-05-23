2be5f83f19e32de2fec0cd7a83da5873936cc2b4
jwichpas <jwichpas@gmail.com>
2025-05-23 12:18:27 -0500

    Se acepto los cambiosque generaban error

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -3,21 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use App\Models\Empresa\EmpresaCondicionesSunat;
 use App\Models\Empresa\EmpresaTributosSunat;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
-=======
->>>>>>> develop
 
 class Empresa extends Model
 {
     protected $table = 'empresas';
     protected $fillable = ['nombre', 'ruc', 'direccion'];
-<<<<<<< HEAD
 
     public function condiciones(): HasOne
     {
@@ -30,6 +26,3 @@ class Empresa extends Model
 }
 
 
-=======
-}
->>>>>>> develop
